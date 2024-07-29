1a1de6dc1901d4823477def0edeceed1861474fb
ilkinbabashov04 <ilkinbabasov99@gmail.com>
2024-07-29 18:06:01 +0400

    Add project files.

---
+++ Core/Data Access/Concrete/BaseReporsitory.cs
@@ -0,0 +1,63 @@
+﻿using Core.Data_Access.Abstract;
+using Core.Entity.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data_Access.Concrete
+{
+    public class BaseReporsitory<TEntity, TContext> : IBaseReporsitory<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var addEntity = context.Entry(entity);
+                addEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deleteEntity = context.Entry(entity);
+                deleteEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+           using TContext context = new TContext();
+            return context.Set<TEntity>().SingleOrDefault(filter);
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using(TContext context = new TContext())
+            {
+                return filter != null ? context.Set<TEntity>().Where(filter).ToList() : context.Set<TEntity>().ToList();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var modifyEntity = context.Entry(entity);
+                modifyEntity.State = EntityState.Modified;
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
